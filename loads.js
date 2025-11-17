a22804373de3a43eaaa529c0988c7c35902f662b
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-11-17 10:24:32 -0600

    113

--- loads.js
+++ loads.js
@@ -110,7 +110,8 @@ const guests = [
   { "id": "109", "name": "Luis Fernando Mejicanos & Narda Pacay de Mejicanos", "passes": 2, "gender": "mixto" },
   { "id": "110", "name": "Ixchel y Raúl", "passes": 2, "gender": "mixto" }, 
   { "id": "111", "name": "Amarilis Rivas", "passes": 1, "gender": "femenino" }, 
-  { "id": "112", "name": "Gary Vasquez", "passes": 1, "gender": "masculino" }
+  { "id": "112", "name": "Gary Vasquez", "passes": 1, "gender": "masculino" }, 
+  { "id": "113", "name": "Fernanda Orozco", "passes": 1, "gender": "femenino" }
 ];
 
 
